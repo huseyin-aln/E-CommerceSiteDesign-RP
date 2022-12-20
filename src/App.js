1c148726f26836c6bc4c3f8f5689fd0322cb0e32
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-20 11:55:55 +0300

    added cart

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Detail />
+      {/* <Detail /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Cart />
     </div>
   );
 }
